d9d134939db775e36637d22ce4268554a9c569da
谢刘建 <377624289@qq.com>
2017-09-15 10:37:22 +0800

    去警告

--- Excel2Unity.py
+++ Excel2Unity.py
@@ -1,34 +1,39 @@
 
 import os
 import xlrd
-from Config import EXCEL_DIR
+from Config import EXCEL_Dir
 
-class Excel2Unity :
+
+class Excel2Unity:
+	# 构造函数
 	def __init__(self):
-		self.mExcelFiles = []	# 所有的excel文件
+		self.mExcelFiles = []		# 所有的excel文件
 
 	# 外部处理函数
 	def process(self):
-		self.recursiveSearchExcel(EXCEL_DIR)
-		self.processClientExcel()
-		self.processServerExcel()
+		self.recursive_searchexcel(EXCEL_Dir)
+		self.process_clientexcel()
+		self.process_serverexcel()
 		print(self.mExcelFiles)
 
 	# 递归查找文件
-	def recursiveSearchExcel(self, path):
-		for dir in os.listdir(path):		# 遍历当前目录
-			fullpath = os.path.join(path, dir)
+	def recursive_searchexcel(self, path):
+		for pathdir in os.listdir(path):		# 遍历当前目录
+			fullpath = os.path.join(path, pathdir)
 
-			if (os.path.isdir(fullpath)):
-				self.recursiveSearchExcel(fullpath)
-			elif(os.path.isfile(fullpath)):
-				if (os.path.splitext(fullpath)[1] == ".xlsx"):
+			if os.path.isdir(fullpath):
+				self.recursive_searchexcel(fullpath)
+			elif os.path.isfile(fullpath):
+				if os.path.splitext(fullpath)[1] == ".xlsx":
 					self.mExcelFiles.append(fullpath)
-
+	
 	# 处理客户端的excel文件
-	def processClientExcel(self):
-		i = 0
+	def process_clientexcel(self):
+		for filename in self.mExcelFiles:
+			filename += ""
+			# data = xlrd.open_workbook(filename)
 
 	# 处理服务器的excel文件
-	def processServerExcel(self):
-		i = 0
+	def process_serverexcel(self):
+		for filename in self.mExcelFiles:
+			filename += ""
